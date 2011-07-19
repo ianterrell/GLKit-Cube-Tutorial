8b7f5d50ddcbc03e4b529c0bc9819c595f69120b
Ian Terrell <ian.terrell@gmail.com>
2011-07-19 12:44:47 -0400

    Initial Commit

---
+++ Cube/CubeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CubeAppDelegate.h
+//  Cube
+//
+//  Created by Ian Terrell on 7/19/11.
+//  Copyright 2011 Ian Terrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CubeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cube/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cube
+//
+//  Created by Ian Terrell on 7/19/11.
+//  Copyright 2011 Ian Terrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CubeAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([CubeAppDelegate class]));
+  }
+}
